f5804f356cb82dc5aeb18523e6148e64da1bec13
王磊 <veryide@qq.com>
2020-05-09 17:52:58 +0800

    init

---
+++ app.js
@@ -0,0 +1,113 @@
+const push = require('umeng-push-server-sdk');
+const conf = require('./conf');
+const umeng = new push.client();
+
+const redis = require('redis');
+const client = redis.createClient(conf['redis.port'], conf['redis.host'], { password: conf['redis.password'] });
+
+var Push = {
+
+	ios: null,
+	and: null,
+	type: null,
+
+	setKey: function (plat, appid, appkey) {
+
+		if (plat == 'iphone') {
+			Push.ios = new push.ios.customizedcast(appid, appkey);
+		}
+
+		if (plat == 'android') {
+			Push.and = new push.android.customizedcast(appid, appkey);
+		}
+
+	},
+
+	setAliasType: function (type) {
+		Push.type = type;
+	},
+
+	sendAndroid: function (msg) {
+
+		Push.and.setAlias(msg.alias, 'guodong_alias');
+		Push.and.setTicker(msg.ticker);
+		Push.and.setTitle(msg.ticker);
+		Push.and.setText(msg.text);
+
+		Push.and.setExtraField('target', msg.after_open);
+		Push.and.setExtraField('content', msg.content);
+		Push.and.setExtraField('category', msg.category);
+
+		Push.and.setMipush(true);
+		//Push.and.setMi_activity('com.guodongbaohe.app.activity.MipushTestActivity');
+
+		Push.and.setDisplayType('notification');
+		Push.and.setProductionMode();
+
+		var ret = umeng.send(Push.and);
+
+		ret.then(function (result) {
+			console.log('Android', '#' + msg.msgid, 'Alias ' + msg.alias, result ? '成功' : '失败');
+			console.log('set DEBUG=* & node app.js');
+		}).catch(function (reason) {
+			console.log('Android', '#' + msg.msgid, reason);
+		});
+
+	},
+
+	sendIPhone: function (msg) {
+
+		Push.ios.setAlias(msg.alias, 'guodong_alias');
+
+		Push.ios.setAlert({
+			'title': msg.ticker,
+			'subtitle': msg.title,
+			'body': msg.text,
+			'after_open': msg.after_open,
+		});
+
+		Push.ios.setCustomizedField('target', msg.after_open);
+		Push.ios.setCustomizedField('content', msg.content);
+		Push.ios.setCustomizedField('category', msg.category);
+
+		Push.ios.setBadge(0);
+		Push.ios.setSound('default');
+
+		var ret = umeng.send(Push.ios);
+
+		ret.then(function (result) {
+			console.log(msg.msgid, 'iPhone 成功');
+		}).catch(function (reason) {
+			console.log(msg.msgid, 'iPhone 失败');
+		});
+
+	},
+
+	send: function () {
+
+		client.on('message', function (channel, message) {
+			//console.log(channel, message)    // test channel-message:"channel message test"
+			var msgs = JSON.parse(message);
+
+			msgs.forEach(msg => {
+				Push.sendAndroid(msg);
+				Push.sendIPhone(msg);
+			});
+
+		})
+
+		client.subscribe('dora_msg_list');
+
+	}
+
+}
+
+//var p = new Push({ 'android': { 'appkey': '123', 'sss': '1111' }, 'iphone': { 'appkey': '123', 'sss': '1111' } });
+
+Push.setAliasType('guodong_alias');
+
+Push.setKey('iphone', '5c075ba2f1f55691f90002aa', '1su76s7veecgz0jr7iwbbys4jg7xsh6j');
+
+Push.setKey('android', '5c075c04b465f599ba000466', 'uq6l3gcmd0juqszj05ga31c8euvokckp');
+
+Push.send();
---
+++ conf.js
@@ -0,0 +1,8 @@
+var conf = {
+	'activity': 'com.guodongbaohe.app.activity.MipushTestActivity',
+	'redis.host': '127.0.0.1',
+	'redis.port': 6379,
+	'redis.password': '',
+}
+
+module.exports = conf;
