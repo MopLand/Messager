214767e19b7ac0b0be8d05ecd8d000d3fb97145e
王磊 <veryide@qq.com>
2020-05-13 22:57:23 +0800

    新增日志上报服务；

--- app.js
+++ app.js
@@ -1,8 +1,16 @@
 
 const System = require('./lib/system');
+const Request = require('./lib/request');
 const Messager = require('./lib/messager');
 
 let conf = System.getConf( __dirname );
 let test = System.getArgv( 'debug' );
 
-var msg = new Messager( conf, test );
+var msg = new Messager( conf, test, (status, msg, chance = 1) => {
+
+	//上报状态信息
+	if( conf.report && System.weight( chance ) ){
+		Request.status( conf.report, 'Messager', status.length, status );
+	}
+
+} );
--- conf.js
+++ conf.js
@@ -3,6 +3,7 @@ var conf = {
 	'android': ['', ''],
 	'aliastype': '',
 	'activity': 'com.guodongbaohe.app.activity.MipushTestActivity',
+	'report': 'http://api.example.com/report',
 	'redis.host': '127.0.0.1',
 	'redis.port': 6379,
 	'redis.password': '',
