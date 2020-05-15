1cba5b4cd7ee957771ad63703466caec72cf3e72
王磊 <veryide@qq.com>
2020-05-15 17:30:04 +0800

    fixed

--- src/account.js
+++ src/account.js
@@ -1,4 +1,5 @@
- 
+'use strict'
+
 const wx = require('../lib/weixin');
 const cm = require('../lib/common');
 
@@ -15,11 +16,23 @@ class Account{
      * 登录
      * @param string member_id 用户ID
      */
-    async login( member_id = '' ) {
+    login( member_id = '' ) {
 
-		var ret = await this.wx.GetLoginQrCode();
+		//var ret = await this.wx.GetLoginQrCode();
 
-		return ret;
+		//return ret;
+
+		//return await this.wx.GetLoginQrCode();
+
+		var pm = this.wx.GetLoginQrCode();
+
+			pm.then( ret => {
+				cm.SavePic( decodeURIComponent( ret.QrBase64 ), 'qr.png' );
+			} );
+
+		return pm;
+
+		//return res.then();
 
 		/*
 		try{
@@ -47,10 +60,17 @@ class Account{
      * @param string wxId 微信Id  必填True 
      * @param string appId 公众号AppId  必填True 
      */
-    async check( uuid ){
+    check( uuid ){
 	
-		var fn = this.wx.CheckLogin( uuid );
-		return fn;
+		var pm = this.wx.CheckLogin( uuid );
+
+			pm.then( ret => {
+				console.log( ret );
+			} ).catch( err => {
+				console.log( err );
+			} );
+
+		return pm;
 	
 			fn.then( ret => {
 	
--- user.js
+++ user.js
@@ -1,3 +1,4 @@
+'use strict'
 
 const Common = require('./lib/common');
 const Account = require('./src/account');
@@ -6,11 +7,36 @@ let conf = Common.getConf( __dirname );
 let step = Common.getArgv( 'step' );
 let user = new Account();
 
+
+/*
+function say() {
+	return new Promise(function(resolve, reject) {
+		setTimeout(function() {
+			let age = 26
+			resolve(`hello, joel。今年我 ${age} 岁`);
+		}, 1000);
+	});
+}
+
+async function demo() {
+	const v = await say(); // 输出：hello, joel。今年我 26 岁  等待这个say 的异步，如果成功把回调 resole 函数的参数作为结果
+	//console.log(typeof v);
+	return v;
+}
+
+console.log( demo() );
+
+return;
+*/
+
 if( step == 'login' ){
 
 	var ret = user.login();
+		ret.then( data => {
+			console.log( data.Uuid, data.ExpiredTime );
+		} )
 
-	//console.log( ret );
+	console.log( ret );
 }
 
 if( step == 'check' ){
