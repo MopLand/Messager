d3b9af80ffad6b664888d2acb3b7a71325c4266f
王磊 <veryide@qq.com>
2020-11-19 16:21:45 +0800

    小程序 appid 提取；

--- src/groups.js
+++ src/groups.js
@@ -500,7 +500,7 @@ class Groups {
 			//小程序，匹配 白名单
 			if( /<appid>/.test( text ) && this.conf.minapp ){
 
-				let appid = /<appid>(.+?)<\/appid>/.exec( text )[1];
+				let appid = /<appid>(?:\<\!\[CDATA\[)?(.+?)(?:\]\]\>)?<\/appid>/.exec( text )[1];
 				let allow = this.conf.minapp.indexOf( appid ) >= 0;
 				
 				log.info('小程序', { 'appid' : appid, 'allow' : allow, 'struct' : text });
