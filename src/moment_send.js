d86564a7864656e10f8598d649193698bd8828e8
王磊 <veryide@qq.com>
2024-07-29 12:45:23 +0800

    POST 提交表单

--- src/moment_send.js
+++ src/moment_send.js
@@ -353,11 +353,11 @@ class MomentSend {
 
 			///////////////
 
-			req.get(self.conf.convert, { 'member_id': member.member_id, 'text': comm.text, 'product': product, 'lazy_time': lazy_time, 'weixin': data.sourced, 'source': 'yfd', 'external': misc }, (code, body) => {
+			//req.get(self.conf.convert, { 'member_id': member.member_id, 'text': comm.text, 'product': product, 'lazy_time': lazy_time, 'weixin': data.sourced, 'source': 'yfd', 'external': misc }, (code, body) => {
 
-			//let pipe = self.conf.convert + '?' + qs.stringify( { 'member_id': member.member_id, 'keyword': comm.exch, 'product': product, 'lazy_time': lazy_time, 'weixin': data.sourced, 'source': 'yfd', 'external': misc } );
+			let pipe = self.conf.convert + '?' + qs.stringify( { 'member_id': member.member_id, 'keyword': comm.exch, 'product': product, 'lazy_time': lazy_time, 'weixin': data.sourced, 'source': 'yfd', 'external': misc } );
 
-			//req.post( pipe, { 'content': comm.text }, (code, body) => {
+			req.form( pipe, { 'content': comm.text }, (code, body) => {
 
 				try {
 					if (typeof body == 'string') {
