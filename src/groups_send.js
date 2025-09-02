18a29ecb4ddea5e8a207e3f5aa28d31ce80c8f29
MopLand <veryide@qq.com>
2025-09-02 14:54:49 +0800

    更新 groups_send.js

--- src/groups_send.js
+++ src/groups_send.js
@@ -39,7 +39,6 @@ class GroupsSend {
 		this.conf = conf;
 		this.wx = new wx(conf.weixin, conf.reserve, conf.special);
 		this.redis = com.redis(conf.redis);
-		//this.sider = com.redis(conf.redis);
 		this.mysql = com.mysql(conf.mysql, (db => { this.mysql = db; }).bind(this));
 
 		this.members = {};
